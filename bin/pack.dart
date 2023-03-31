4cf7ad4d33137302ffe474b7d447e320d02aac93
prathiv <prathiv.ar@grootan.com>
2023-03-31 14:03:28 +0530

    Display password along with length in log

--- bin/pack.dart
+++ bin/pack.dart
@@ -11,7 +11,7 @@ Future<void> main(List<String> arguments) async {
   final outputFilePath = arguments[1];
   final password = arguments[2];
   if (password.length != 16) {
-    print("Password length must be 16");
+    print("Password length must be 16 given password $password");
     exit(1);
   }
   final assetDir = Directory(assetPath);
